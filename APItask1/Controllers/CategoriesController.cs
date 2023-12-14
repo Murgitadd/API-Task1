bc97134c14d05e8d5462d4a52c0f2783b4142f06
Murad Xasaddinov <murad.xasaddinov@gmail.com>
2023-12-14 23:40:50 +0400

    initial commit

---
+++ APItask1/Controllers/CategoriesController.cs
@@ -0,0 +1,81 @@
+﻿using APItask1.DAL;
+using APItask1.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APItask1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get(int page=1,int take=3)
+        {
+            List<Category> categories = await _context.Categories.Skip((page-1)*take).Take(take).ToListAsync();
+            return Ok(categories);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            if (id <= 0)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+            Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (category == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+            return StatusCode(StatusCodes.Status200OK, category);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, string name)
+        {
+
+            if (id <= 0)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+            Category existed = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (existed == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+            existed.Name = name;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete (int id)
+        {
+
+            if (id <= 0)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+            Category existed = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (existed == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+            _context.Categories.Remove(existed);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ APItask1/DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APItask1.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace APItask1.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
